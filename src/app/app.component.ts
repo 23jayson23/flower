919ab18c0a13ee9bef94439b716bc29a02bb94e2
23jayson23 <reyes.jayson5282@gmail.com>
2023-10-15 17:07:23 +0800

    new

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent{
   title = 'flower';
+  hide = false
+
+
+  onload = () => {
+    const c = setTimeout(() => {
+      document.body.classList.remove("not-loaded");
+      clearTimeout(c);
+    }, 1000);
+  };
+
+  open(){
+    this.hide = true
+  }
 }
+
